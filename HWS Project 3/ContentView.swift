2af9bb5829a65e171a5f7769176c774f5c0c85a2
R. Kukuh <rkukuh@gmail.com>
2024-10-24 06:35:35 +0700

    02

--- HWS Project 3/ContentView.swift
+++ HWS Project 3/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
         }
-        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(.red)
     }
 }
