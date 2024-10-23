8c412fc084296e15f3e10495e4e3823031f4b733
R. Kukuh <rkukuh@gmail.com>
2024-10-24 06:24:42 +0700

    Initial Commit

---
+++ HWS Project 3/HWS_Project_3App.swift
@@ -0,0 +1,17 @@
+//
+//  HWS_Project_3App.swift
+//  HWS Project 3
+//
+//  Created by R. Kukuh on 24/10/24.
+//
+
+import SwiftUI
+
+@main
+struct HWS_Project_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
