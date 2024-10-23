29cbc566ba83cb94ec9037d37da6cbccd7091f61
R. Kukuh <rkukuh@gmail.com>
2024-10-24 06:44:17 +0700

    04

--- HWS Project 3/ContentView.swift
+++ HWS Project 3/ContentView.swift
@@ -10,11 +10,15 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Button("Hello, world!") {
-                print(type(of: self.body))
-            }
-            .frame(width: 200, height: 200)
-            .background(.red)
+            Text("Hello, world!")
+                .padding()
+                .background(.red)
+                .padding()
+                .background(.blue)
+                .padding()
+                .background(.green)
+                .padding()
+                .background(.yellow)
         }
         .padding()
     }
