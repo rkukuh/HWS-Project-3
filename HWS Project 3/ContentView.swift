69cc1f377547132422558449d342fac2b6994c8b
R. Kukuh <rkukuh@gmail.com>
2024-10-24 09:58:58 +0700

    01

--- HWS Project 3/ContentView.swift
+++ HWS Project 3/ContentView.swift
@@ -9,18 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Hello, world!")
-                .padding()
-                .background(.red)
-                .padding()
-                .background(.blue)
-                .padding()
-                .background(.green)
-                .padding()
-                .background(.yellow)
-        }
-        .padding()
+        
+        Text("Hello, SwiftUI")
+        
     }
 }
 
